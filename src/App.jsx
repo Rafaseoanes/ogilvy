0704d5f222ece7832206a5153e94e2a98bd96fae
Rafael Seoanes <rafaseoanes9@gmail.com>
2023-01-29 13:44:35 -0500

    Style updated

--- src/App.jsx
+++ src/App.jsx
@@ -5,12 +5,14 @@ const App = () => {
     <div>
       <div className="navbar">
         <img style={{ 'height': '30px' }} src="https://i.imgur.com/dGpH2Gr.png" alt="Ogilvy logo" />
-        <li>
-          <ul>HOME</ul>
-          <ul>ABOUT</ul>
-          <ul>COURSES</ul>
-          <ul>BLOG</ul>
-        </li>
+        <div className="topList">
+          <li>
+            <ul>HOME</ul>
+            <ul>ABOUT</ul>
+            <ul>COURSES</ul>
+            <ul>BLOG</ul>
+          </li>
+        </div>
         <button style={{ 'height': '25px', 'border': 'none', 'backgroundColor': 'white', 'width': '75px' }} >SIGN IN</button>
       </div>
       <div className="section "></div>
