7890e18c18d4110bbf2b60f0ef411c581dd2497d
Rafael Seoanes <rafaseoanes9@gmail.com>
2023-01-29 13:28:38 -0500

    Navbar created, styles added

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,16 @@ import React from "react";
 const App = () => {
   return (
     <div>
-      <div className="navbar"></div>
+      <div className="navbar">
+        <img style={{ 'height': '30px' }} src="https://i.imgur.com/dGpH2Gr.png" alt="Ogilvy logo" />
+        <li>
+          <ul>HOME</ul>
+          <ul>ABOUT</ul>
+          <ul>COURSES</ul>
+          <ul>BLOG</ul>
+        </li>
+        <button style={{ 'height': '25px', 'border': 'none', 'backgroundColor': 'white', 'width': '75px' }} >SIGN IN</button>
+      </div>
       <div className="section "></div>
       <div className="section "></div>
       <div className="section "></div>
@@ -12,7 +21,7 @@ const App = () => {
       <div className="section "></div>
       <div className="section "></div>
       <div className="section "></div>
-      <div className="section "></div>
+      <div className="footer "></div>
     </div>
   );
 };
