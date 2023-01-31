841f18686637e13d88fe05b529c668df22f9a374
Rafael Seoanes <rafaseoanes9@gmail.com>
2023-01-30 21:29:18 -0500

    carrousel added

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,35 @@
 import React from "react";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const App = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    initialSlide: 0,
+    responsive: [
+      {
+        breakpoint: 900,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          initialSlide: 1
+        }
+      }]
+  };
   return (
     <div>
       <div className="navbar">
@@ -20,7 +49,7 @@ const App = () => {
           <div className="heroFraction fractionOne">
             <h3 className="uiText">UI<span className="uiPoint">.</span></h3>
             <h4 className="beginnerText">BEGINNER'<span className="beginnerS">s</span></h4>
-            <h5>■ BECOME A <br /> PRO-DESIGNER</h5>
+            <h5>■ BECOME A <br />&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; PRO-DESIGNER</h5>
           </div>
           <div className="heroFraction fractionTwo">
             <img style={{ 'height': '80vh' }} src="https://i.imgur.com/kW00Dc0.png" alt="developer" />
@@ -54,19 +83,105 @@ const App = () => {
       </div>
       <div className="section ">
         <div className="skills">
-          <h4 className="skillsRight">■ DISCOVER YOUR SKILLS</h4>
+          <h4 className="skillsRight">■ &nbsp;&nbsp;DISCOVER YOUR SKILLS</h4>
           <div className="skillsCenter">
             <h2 style={{ 'textAlign': 'center', 'fontSize': '60px', 'marginTop': '220px' }}>ACCEPT <br /> <span className="thinText">NEW</span> <br /> CHALLENGES</h2>
             <img id="atomShape" src="https://i.imgur.com/NumISy0.png" alt="shapes" />
           </div>
-          <h4 className="skillsLeft">■ DISCOVER YOUR SKILLS</h4>
+          <h4 className="skillsLeft">■ &nbsp;&nbsp;DISCOVER YOUR SKILLS</h4>
         </div>
         <div className="skillsAbout">
           <h2>EXPLORE</h2>
           <h2>ABOUT</h2>
         </div>
       </div>
-      <div className="section "></div>
+      <div className="section ">
+        <div className="coursesTitle">
+          <h5><span style={{ 'fontSize': '65px', 'fontWeight': '700' }}>FIND </span > <span style={{ 'fontSize': '25px', 'fontWeight': '300', 'letterSpacing': '4px' }}> Your Course.</span></h5>
+          <h5>■ <span style={{ 'fontSize': '10px', 'fontWeight': '300' }}> EDITION</span><br /><span>&nbsp;&nbsp;&nbsp;2023</span></h5>
+        </div>
+
+        {/* carrousel */}
+        <div style={{ 'textAlign': 'center', 'padding': '65px 150px 30px 150px' }}>
+          <Slider {...settings}>
+
+            <div className="carrouselCard">
+              <div className="cornerNumber">.01</div>
+              <h3>COURSE <span className="uiPoint">01</span></h3>
+              <p style={{ 'color': '#a460f9', 'fontWeight': '700' }}>_____</p>
+
+              <br />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ducimus facilis asperiores? Corporis fugiat.</p>
+
+              <br />
+              <button style={{ 'height': '32px', 'margin': '5px', 'color': 'white', 'border': 'none', 'backgroundColor': '#a460f9', 'width': '95px' }}>READ MORE</button>
+            </div>
+
+
+
+            <div className="carrouselCard">
+              <div className="cornerNumber">.02</div>
+              <h3>COURSE <span className="uiPoint">02</span></h3>
+              <p style={{ 'color': '#a460f9', 'fontWeight': '700' }}>_____</p>
+
+              <br />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ducimus facilis asperiores? Corporis fugiat.</p>
+
+              <br />
+              <button style={{ 'height': '32px', 'margin': '5px', 'color': 'white', 'border': 'none', 'backgroundColor': '#a460f9', 'width': '95px' }}>READ MORE</button>
+            </div>
+
+            <div className="carrouselCard">
+              <div className="cornerNumber">.03</div>
+              <h3>COURSE <span className="uiPoint">03</span></h3>
+              <p style={{ 'color': '#a460f9', 'fontWeight': '700' }}>_____</p>
+
+              <br />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ducimus facilis asperiores? Corporis fugiat.</p>
+
+              <br />
+              <button style={{ 'height': '32px', 'margin': '5px', 'color': 'white', 'border': 'none', 'backgroundColor': '#a460f9', 'width': '95px' }}>READ MORE</button>
+            </div>
+
+            <div className="carrouselCard">
+              <div className="cornerNumber">.04</div>
+              <h3>COURSE <span className="uiPoint">04</span></h3>
+              <p style={{ 'color': '#a460f9', 'fontWeight': '700' }}>_____</p>
+
+              <br />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ducimus facilis asperiores? Corporis fugiat.</p>
+
+              <br />
+              <button style={{ 'height': '32px', 'margin': '5px', 'color': 'white', 'border': 'none', 'backgroundColor': '#a460f9', 'width': '95px' }}>READ MORE</button>
+            </div>
+
+            <div className="carrouselCard">
+            <div className="cornerNumber">.05</div>
+              <h3>COURSE <span className="uiPoint">05</span></h3>
+              <p style={{ 'color': '#a460f9', 'fontWeight': '700' }}>_____</p>
+              
+              <br />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ducimus facilis asperiores? Corporis fugiat.</p>
+             
+              <br />
+              <button style={{ 'height': '32px', 'margin': '5px', 'color': 'white', 'border': 'none', 'backgroundColor': '#a460f9', 'width': '95px' }}>READ MORE</button>
+            </div>
+
+            <div className="carrouselCard">
+            <div className="cornerNumber">.06</div>
+              <h3>COURSE <span className="uiPoint">06</span></h3>
+              <p style={{ 'color': '#a460f9', 'fontWeight': '700' }}>_____</p>
+              
+              <br />
+              <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Deserunt ducimus facilis asperiores? Corporis fugiat.</p>
+              
+              <br />
+              <button style={{ 'height': '32px', 'margin': '5px', 'color': 'white', 'border': 'none', 'backgroundColor': '#a460f9', 'width': '95px' }}>READ MORE</button>
+            </div>
+
+          </Slider>
+        </div>
+      </div>
       <div className="section "></div>
       <div className="section "></div>
       <div className="section "></div>
