51a28044cbc7e708875275299ef5d7701212ecdf
Rafael Seoanes <rafaseoanes9@gmail.com>
2023-01-31 22:11:42 -0500

    Sections added

--- src/App.jsx
+++ src/App.jsx
@@ -233,9 +233,26 @@ const App = () => {
         </div>
         <h5 style={{'lineHeight': '17px'}} className="skillsRight"> <span style={{'fontSize': '40px', 'fontWeight':'300'}}>&#10230; </span> READY TO <br /> &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  GET STARTED</h5>
       </div>
-      <div className="section "></div>
-      <div className="section "></div>
-      <div className="section"></div>
+      <div>
+        <div className="aboutUs">
+        <div>
+            <img style={{ 'width': '50vw', 'position': 'relative', 'left': '-200px', 'bottom': '-25px'}} src="https://i.imgur.com/Bd4ANxi.png" alt="about" />
+          </div>
+          <div>
+            <h5 style={{'fontSize': '60px'}} >Why <span style={{ 'fontWeight': '300'}} className="uiPoint">us.</span></h5>
+            <br />
+            <br />
+            <br />
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae vel soluta ex quae ut, commodi corporis debitis a nobis provident consequatur qui, error doloribus impedit tempora sint alias aliquam. Quia. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ducimus culpa corporis natus temporibus est veniam esse nesciunt! Assumenda, quaerat tempora repellendus, ipsum ipsam sequi hic minima eligendi, suscipit sunt corporis!</p>
+          <br />
+          <br />
+          <br />
+          <button style={{ 'height': '32px', 'margin': '5px', 'color': 'white', 'border': 'none', 'backgroundColor': '#a460f9', 'width': '95px' }}>READ MORE</button>
+
+          </div>
+          
+        </div>
+      </div>
       <div className="footer ">
         <div>
           <ul style={{ 'listStyle': 'none' }}>
